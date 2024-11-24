064703583e2991e7eb4fdbe39f7eb96db90bba7d
Phaylali <admin@omniversify.com>
2024-11-24 06:21:17 +0100

    update site completely

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,8 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+
 export default defineConfig({
-  site: "https://astro-moon-landing.netlify.app/",
+  site: "https://omniversify.com/",
   integrations: [tailwind(), icon()],
 });
